36cf77737cfdc11241d7ee09f53c127f9aa0d553
rohinishraj124 <rohinishraj124@gmail.com>
2024-12-30 10:38:25 +0530

    small changes in footer

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -5,8 +5,8 @@ import Link from 'next/link';
 const Footer = () => {
     return (
         <footer className="text-gray-600 body-font bg-[#ededed]">
-            <div className='bg-[#aba4a4] h-0.5 w-full rounded-full'></div>
-            <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+            <div className="bg-[#aba4a4] h-0.5 w-full rounded-full"></div>
+            <div className="container px-5 py-24 mx-auto grid grid-cols-1 sm:grid-cols-2 gap-4">
                 <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
                     <Link href="/" className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
                         {/* <Image src="/logonew.png" alt="Logo" width={50} height={50}></Image> */}
@@ -28,8 +28,8 @@ const Footer = () => {
                         Your one-stop online shop for amazing deals and unique finds.
                     </p>
                 </div>
-                <div className="flex-grow flex flex-wrap sm:pl-20 -mb-10 sm:mt-0 mt-10 sm:text-left text-center">
-                    <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+                <div className="sm:col-span-1 grid grid-cols-2 gap-[5em] m-auto lg:grid-cols-4 md:text-left ">
+                    <div>
                         <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
                         <nav className="list-none mb-10">
                             <li>
@@ -41,15 +41,9 @@ const Footer = () => {
                             <li>
                                 <Link href="/kids" className="text-gray-600 hover:text-pink-800">Kids</Link>
                             </li>
-                            <li>
-                                {/* <Link href="/home_living" className="text-gray-600 hover:text-pink-800">Home & Living</Link>
-                            </li>
-                            <li>
-                                <Link href="/beauty" className="text-gray-600 hover:text-pink-800">Beauty</Link> */}
-                            </li>
                         </nav>
                     </div>
-                    <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+                    <div>
                         <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">ABOUT</h2>
                         <nav className="list-none mb-10">
                             <li>
@@ -66,7 +60,7 @@ const Footer = () => {
                             </li>
                         </nav>
                     </div>
-                    <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+                    <div>
                         <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">SUPPORT</h2>
                         <nav className="list-none mb-10">
                             <li>
@@ -83,7 +77,7 @@ const Footer = () => {
                             </li>
                         </nav>
                     </div>
-                    <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+                    <div>
                         <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">FOLLOW US</h2>
                         <nav className="list-none mb-10">
                             <li>
@@ -111,61 +105,7 @@ const Footer = () => {
                         </Link>
                     </p>
                     <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-                        <Link href="#" className="text-gray-500">
-                            <svg
-                                fill="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                className="w-5 h-5"
-                                viewBox="0 0 24 24"
-                            >
-                                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                            </svg>
-                        </Link>
-                        <Link href="#" className="ml-3 text-gray-500">
-                            <svg
-                                fill="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                className="w-5 h-5"
-                                viewBox="0 0 24 24"
-                            >
-                                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                            </svg>
-                        </Link>
-                        <Link href="#" className="ml-3 text-gray-500">
-                            <svg
-                                fill="none"
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                className="w-5 h-5"
-                                viewBox="0 0 24 24"
-                            >
-                                <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                                <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-                            </svg>
-                        </Link>
-                        <Link href="#" className="ml-3 text-gray-500">
-                            <svg
-                                fill="currentColor"
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="0"
-                                className="w-5 h-5"
-                                viewBox="0 0 24 24"
-                            >
-                                <path
-                                    stroke="none"
-                                    d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
-                                ></path>
-                                <circle cx="4" cy="4" r="2" stroke="none"></circle>
-                            </svg>
-                        </Link>
+                        {/* Social Media Icons */}
                     </span>
                 </div>
             </div>
